8f5cdb42da9378b7c7ef44978bca3aa94b11b29a
Marco Cipri <mcipri@IT01MN00002.local>
2018-12-18 18:41:43 +0100

    test whit long list... whit trick...

--- src/flatten_list/tests/basic_test.py
+++ src/flatten_list/tests/basic_test.py
@@ -9,7 +9,7 @@ Copyright 2018, Marco Cipri
 Licensed under MIT
 '''
 
-#from iteration_utilities import deepflatten
+from iteration_utilities import deepflatten
 from flatten_list import deep_flat
 import pytest
 
@@ -28,5 +28,5 @@ def test_passes():
         deep_flat(main_list) 
 
 def test_long():
-    main_list = [1,2,[21,22],[31,32,[334,335, [3351,3352,3353],336,[3361,3362,3363]],4,list(range(100))]]
-    assert deep_flat(main_list) == [1,2,21,22,31,32,334,335,3351,3352,3353,336,3361,3362,3363,4,list(range(100))]
+    main_list = [1,2,[21,22],[31,32,[334,335, [3351,3352,3353],336,[3361,3362,3363]],4,list(range(100000))]]
+    assert deep_flat(main_list) == list(deepflatten(main_list))
