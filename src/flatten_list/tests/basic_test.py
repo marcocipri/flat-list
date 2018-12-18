00608bcaa9684f748534eb4a0988cba753dd9d91
Marco Cipri <mcipri@IT01MN00002.local>
2018-12-18 21:20:21 +0100

    more unit test

--- src/flatten_list/tests/basic_test.py
+++ src/flatten_list/tests/basic_test.py
@@ -22,11 +22,32 @@ def test_complex():
     main_list = [1,2,[21,22],[31,32,[334,335, [3351,3352,3353],336,[3361,3362,3363]],4]]
     assert deep_flat(main_list) == [1,2,21,22,31,32,334,335,3351,3352,3353,336,3361,3362,3363,4]
     
-def test_passes():
+def test_string():
     with pytest.raises(Exception) as e_info:
         main_list = [1,2,[21,22],[31,32,[334,335, [3351,3352,3353],336,[3361,3362,3363]],4, "33",list(range(100))]]
         deep_flat(main_list) 
 
-def test_long():
-    main_list = [1,2,[21,22],[31,32,[334,335, [3351,3352,3353],336,[3361,3362,3363]],4,list(range(100000))]]
+def test_char():
+    with pytest.raises(Exception) as e_info:
+        main_list = [1,2,[21,22],[31,32,[334,335, [3351,3352,3353],336,[3361,3362,3363]],4, s ,list(range(100))]]
+        deep_flat(main_list) 
+
+
+def test_dict():
+    with pytest.raises(Exception) as e_info:
+        main_list = [1,2,[21,22],[31,32,[334,335, [3351,3352,3353],336,[3361,3362,3363]],4, {6,7} ,list(range(100))]]
+        deep_flat(main_list) 
+
+def test_float():
+    with pytest.raises(Exception) as e_info:
+        main_list = [1,2,[21,22],[31,3.2,[334,335, [3351,3352,3353],336,[3361,3362,3363]],4 ,list(range(100))]]
+        deep_flat(main_list) 
+
+def test_long_list():
+    main_list = [1,2,[21,22],[31,32,[334,335, [3351,3352,3353],336,[3361,3362,3363]],4,list(range(100))]]
+    assert deep_flat(main_list) == list(deepflatten(main_list))
+
+
+def test_more_long_string():
+    main_list = [1,2,[21,22],[31,32,[334,335, [3351,3352,3353],336,[3361,3362,3363]],4,list(range(100))]]
     assert deep_flat(main_list) == list(deepflatten(main_list))
