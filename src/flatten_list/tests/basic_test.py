2522aa39dbb717358aa146badea292b0fa60a944
Marco Cipri <mcipri@IT01MN00002.local>
2018-12-19 16:49:16 +0100

    add test_extremely_long_string

--- src/flatten_list/tests/basic_test.py
+++ src/flatten_list/tests/basic_test.py
@@ -51,3 +51,7 @@ def test_long_list():
 def test_more_long_string():
     main_list = [1,2,[21,22],[31,32,[334,335, [3351,3352,3353],336,[3361,3362,3363]],4,list(range(100))]]
     assert deep_flat(main_list) == list(deepflatten(main_list))
+
+def test_extremely_long_string():
+    main_list = [1,2,[21,22],[31,32,[334,list(range(20000,30000)), [3351,3352,3353],list(range(10000,20000)),[3361,list(range(220000,230000)),3363]],4,list(range(40000,60000))]]
+    assert deep_flat(main_list) == list(deepflatten(main_list))
